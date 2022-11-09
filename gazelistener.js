8e635e65887a384cdae6b342d4c82144c3825613
Alessio Alfieri <supermediatablet@gmail.com>
2022-11-09 17:28:48 +0100

    revert again

--- gazelistener.js
+++ gazelistener.js
@@ -1,155 +1,170 @@
 var elements = [];
-var hasBeenOver = [];
+var hasBeenOver = []
 var hasLeft = new Set();
 var counter = 0;
 var buffer = 0;
-var radius = [0, 0];
-const predictionTimer = 1; // Interval of time in ms that the function is run
-const numStoredPoints = 7; // Amount of previous points the program takes into account
-var locHistory = []; // Array that holds previous locations
-var index = 0; // Index for locHistory
-var cx,
-  cy,
-  vx,
-  vy = 0; // Cursor varaibles (update to class if necessary)
+var radius = [0,0];
+const predictionTimer = 1;      // Interval of time in ms that the function is run
+const numStoredPoints = 7;     // Amount of previous points the program takes into account
+var locHistory = [];            // Array that holds previous locations
+var index = 0;                  // Index for locHistory
+var cx, cy, vx, vy = 0;         // Cursor varaibles (update to class if necessary)
 var CursorDrawn = false;
 
-function emptyList() {
-  elements.length = 0;
+
+
+function emptyList(){
+    elements.length = 0;
 }
-function addElement(id) {
-  elements.push(id);
+function addElement(id){
+    elements.push(id);
+    
 }
 console.log(elements);
-function isOverListener() {
-  var prediction = webgazer.getCurrentPrediction();
-  if (prediction) {
-    let x = prediction.x;
-    let y = prediction.y;
+function isOverListener()
+{
+    var prediction = webgazer.getCurrentPrediction();
+    if (prediction) 
+    {
+        let x = prediction.x;
+        let y = prediction.y;
 
-    hasBeenOver[counter] = null;
-    elements.forEach(function (id) {
-      const ele = document.getElementById(id);
-      const eleStyle = window.getComputedStyle(ele, null);
-      let widthStr = eleStyle.width.toString();
-      //console.log(widthStr);
-      let width = parseInt(widthStr.substring(0, widthStr.length - 2)) + buffer;
-      let heightStr = eleStyle.height.toString();
-      let height =
-        parseInt(heightStr.substring(0, heightStr.length - 2)) + buffer;
-      let topStr = eleStyle.top.toString();
-      let top = parseInt(topStr.substring(0, topStr.length - 2)) - buffer;
-      let leftStr = eleStyle.left.toString();
-      let left = parseInt(leftStr.substring(0, leftStr.length - 2)) - buffer;
-      //console.log("left: "+left+"top:"+top+"height: "+height+"width: "+width);
-      let left_side = cx - radius[0];
-      let right_side = cx + radius[0];
-      let bottom_side = cy - radius[1];
-      let top_side = cy + radius[1];
-      let inside =
-        ((left < left_side && left_side < left + width) ||
-          (left < right_side && right_side < left + width)) &&
-        ((top < top_side && top_side < top + height) ||
-          (top < bottom_side && bottom_side < top + height));
-      //let inside = x > left && y > top && y < top+height && x < left + width;
-      //console.log(inside);
-
-      if (inside) {
-        if (hasBeenOver.includes(id)) {
-          ele.classList.add("hover");
-        } else {
-          hasBeenOver[counter] = id;
-        }
-      } else {
-        if (hasLeft.has(id)) {
-          hasLeft.delete(id);
-          ele.classList.remove("hover");
-        } else {
-          hasLeft.add(id);
-        }
-      }
-    });
-    counter = counter + (1 % 10);
-  }
+        hasBeenOver[counter] = null;
+        elements.forEach(function(id){
+            const ele = document.getElementById(id);
+            const eleStyle = window.getComputedStyle(ele, null)
+            let widthStr = eleStyle.width.toString();
+            //console.log(widthStr);
+            let width = parseInt(widthStr.substring(0,widthStr.length - 2)) + buffer; 
+            let heightStr = eleStyle.height.toString();
+            let height = parseInt(heightStr.substring(0,heightStr.length-2)) + buffer;
+            let topStr = eleStyle.top.toString();
+            let top = parseInt(topStr.substring(0,topStr.length-2)) - buffer;
+            let leftStr = eleStyle.left.toString();
+            let left = parseInt(leftStr.substring(0,leftStr.length-2)) - buffer;
+            //console.log("left: "+left+"top:"+top+"height: "+height+"width: "+width);
+            let left_side = cx - radius[0];
+            let right_side = cx + radius[0];
+            let bottom_side = cy - radius[1];
+            let top_side = cy + radius[1];
+            let inside = ((left < left_side && left_side < left + width) || (left < right_side && right_side < left + width)) && ((top < top_side && top_side < top + height) || (top < bottom_side && bottom_side < top + height));
+            //let inside = x > left && y > top && y < top+height && x < left + width;
+            //console.log(inside);
+            
+            if(inside)
+            {
+                if(hasBeenOver.includes(id))
+                {
+                    ele.classList.add("hover");
+                  
+                }
+                else
+                {
+                    hasBeenOver[counter] = id;
+                }
+            }
+            else
+            {
+                if(hasLeft.has(id))
+                {
+                    hasLeft.delete(id)
+                    ele.classList.remove("hover");
+                }
+                else
+                {
+                    hasLeft.add(id);
+                }
+            }
+        });
+        counter= counter + 1 % 10;   
+        
+    
+    }
+   
 }
 
 // Takes array of points and returns the average.
-function averagePoints(points) {
-  average = [0, 0];
-  for (i = 0; i < points.length; i++) {
-    average[0] += points[i][0];
-    average[1] += points[i][1];
-  }
-  newx = average[0] / points.length;
-  newy = average[1] / points.length;
-  return [newx, newy];
+function averagePoints(points)
+{
+    average = [0,0];
+    for (i = 0; i < points.length; i++)
+    {
+        average[0] += points[i][0];
+        average[1] += points[i][1];
+    }
+    newx = average[0] / points.length;
+    newy = average[1] / points.length;
+    return [newx, newy];
 }
 
 // Takes array of points, their average, and returns the standard deviation of X and Y.
-function standardDeviation(points, avg) {
-  temp = [0, 0];
+function standardDeviation(points, avg)
+{
+    temp = [0,0];
 
-  for (i = 0; i < points.length; i++) {
-    temp[0] += (points[i][0] - avg[0]) ** 2;
-    temp[1] += (points[i][1] - avg[1]) ** 2;
-  }
+    for (i = 0; i < points.length; i++)
+    {
+        temp[0] += (points[i][0] - avg[0]) ** 2;
+        temp[1] += (points[i][1] - avg[1]) ** 2;
+    }
 
-  sX = Math.sqrt(temp[0] / points.length);
-  sY = Math.sqrt(temp[1] / points.length);
+    sX = Math.sqrt(temp[0] / points.length);
+    sY = Math.sqrt(temp[1] / points.length);
 
-  return [sX, sY];
+    return [sX, sY];
 }
 
 // Draws an indicator showing where the user is looking
-function drawCursor() {
-  var eyeCursor = document.createElement("div");
-  eyeCursor.style.borderRadius = "100%";
-  eyeCursor.style.height = "50px";
-  eyeCursor.style.width = "50px";
-  eyeCursor.style.boxShadow = "inset 0 0 10px 0 rgba(255, 255, 255, 0.8)";
-  eyeCursor.style.position = "absolute";
-  eyeCursor.style.zIndex = "9999";
-  eyeCursor.id = "eyeCursor";
-  eyeCursor.style.left = 0;
-  eyeCursor.style.top = 0;
+function drawCursor()
+{
+    var eyeCursor = document.createElement("div");
+    eyeCursor.style.borderRadius = "100%";
+    eyeCursor.style.height = "50px";
+    eyeCursor.style.width = "50px";
+    eyeCursor.style.boxShadow = "inset 0 0 10px 0 rgba(255, 255, 255, 0.8)";
+    eyeCursor.style.position = "absolute";
+    eyeCursor.style.zIndex = "9999";
+    eyeCursor.id = "eyeCursor";
+    eyeCursor.style.left = 0;
+    eyeCursor.style.top = 0;
 
-  document.body.appendChild(eyeCursor);
-  console.log("CURSOR DRAWN!");
+    document.body.appendChild(eyeCursor);
+    console.log("CURSOR DRAWN!");
 }
 
 // Updates the eye cursor's coordinates
 //var prevPosition = [0,0];
 //var vx, vy = 0;
-function updateCursor(point, circleSize) {
-  var minRadius = 100;
-  var eyeCursor = document.getElementById("eyeCursor");
+function updateCursor(point, circleSize)
+{
+    var minRadius = 100;
+    var eyeCursor = document.getElementById("eyeCursor");
 
-  if (!CursorDrawn) {
-    // Adds the cursor Element to the webpage
-    drawCursor();
-    CursorDrawn = true;
-  }
+    if (!CursorDrawn){          // Adds the cursor Element to the webpage
+        drawCursor();
+        CursorDrawn = true;
+    }
 
-  // Radius Calculations
-  radius[0] = 10 * Math.sqrt(circleSize[0]);
-  radius[1] = 10 * Math.sqrt(circleSize[1]);
+    // Radius Calculations
+    radius[0] = 10 * Math.sqrt(circleSize[0]);
+    radius[1] = 10 * Math.sqrt(circleSize[1]);
 
-  //console.log("Radius: "+radius);
+    //console.log("Radius: "+radius);
 
-  if (radius[0] < minRadius) {
-    radius[0] = minRadius;
-  }
-  if (radius[1] < minRadius) {
-    radius[1] = minRadius;
-  }
+    if(radius[0] < minRadius){
+        radius[0] = minRadius;
+    }
+    if(radius[1] < minRadius){
+        radius[1] = minRadius;
+    }
 
-  eyeCursor.style.width = radius[0] + "px";
-  eyeCursor.style.height = radius[1] + "px";
+    eyeCursor.style.width = radius[0]+"px";
+    eyeCursor.style.height = radius[1]+"px";
 
-  cx = point[0] - radius[0];
-  cy = point[1] - radius[1];
+    cx = point[0] - radius[0];
+    cy = point[1] - radius[1];
 
-  /*
+    /*
     var w = window.innerWidth;
     var h = window.innerHeight;
 
@@ -169,11 +184,12 @@ function updateCursor(point, circleSize) {
     }
     */
 
-  eyeCursor.style.left = cx + "px";
-  eyeCursor.style.top = cy + "px";
+    eyeCursor.style.left = (cx) + "px";
+    eyeCursor.style.top = (cy) + "px";
 
-  // Position Calculations
-  /*var acc = 10;
+    
+    // Position Calculations
+    /*var acc = 10;
     (point[0] > prevPosition[0]) ? (vx += acc): (vx -= acc);         // If looking righter than cursor, increase x vel, else decrease x vel
     (point[1] > prevPosition[1]) ? (vy += acc): (vy -= acc);         // If looking higher than cursor, increase y vel, else decrease y vel
 
@@ -185,24 +201,27 @@ function updateCursor(point, circleSize) {
 }
 
 // Provide a better prediction of where the user is looking & draw cursor
-function betterPrediction() {
-  var prediction = webgazer.getCurrentPrediction();
-  if (prediction) {
-    let x = prediction.x; // Webgazer x prediction
-    let y = prediction.y; // Webgazer y prediction
-    locHistory[index] = [x, y]; // Store coordinates in a list
+function betterPrediction()
+{
+    var prediction = webgazer.getCurrentPrediction();
+    if (prediction) 
+    {
+        let x = prediction.x;                       // Webgazer x prediction
+        let y = prediction.y;                       // Webgazer y prediction
+        locHistory[index] = [x,y];                  // Store coordinates in a list
+        
+        newCoords = averagePoints(locHistory)       // Take the average of all the points
+        
 
-    newCoords = averagePoints(locHistory); // Take the average of all the points
+        circleSize = standardDeviation(locHistory, newCoords);      // Find standard deviation of points and adjust cursor size to fit
+        //console.log("X "+x+" Y "+y);
+        //console.log("Avg coords: "+newCoords);
+        //console.log("SD: "+circleSize);
 
-    circleSize = standardDeviation(locHistory, newCoords); // Find standard deviation of points and adjust cursor size to fit
-    //console.log("X "+x+" Y "+y);
-    //console.log("Avg coords: "+newCoords);
-    //console.log("SD: "+circleSize);
+        updateCursor(newCoords, circleSize);                    // Adjust cursor to match new data
 
-    updateCursor(newCoords, circleSize); // Adjust cursor to match new data
-
-    index = (index + 1) % numStoredPoints; // Increment index (loops back to 0)
-  }
+        index = (index + 1) % numStoredPoints;      // Increment index (loops back to 0)
+    }
 }
 
 //window.onload = function() {
--- main.js
+++ main.js
@@ -1,196 +1,203 @@
-window.onload = function () {
-  //start the webgazer tracker
-  webgazer
-    .setRegression("ridge") /* currently must set regression and tracker */
-    .setTracker("clmtrackr")
-    .setGazeListener(function () {})
-    .begin()
-    .showPredictionPoints(
-      true
-    ); /* shows a square every 100 milliseconds where current prediction is */
-  setInterval(function () {
-    document.getElementById("text").style.opacity = 100;
-  }, 4000);
-};
+window.onload = function() {
+    //start the webgazer tracker
+    webgazer.setRegression('ridge') /* currently must set regression and tracker */
+        .setTracker('clmtrackr')
+        .setGazeListener(function() {
+
+        })
+        .begin()
+        .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
+        setInterval(function(){
+            document.getElementById("text").style.opacity = 100
+        }, 4000)
+}
 
 var myMap = new Map();
-function addToMap(string, func) {
-  myMap.set(string, func);
-  console.log(myMap);
+function addToMap(string, func)
+{
+    myMap.set(string,func);
+    console.log(myMap);
 }
 
-var validator = 0;
-setInterval(function () {
-  prediction = webgazer.getCurrentPrediction();
-  console.log(document.elementFromPoint(prediction.x, prediction.y));
-  if (
-    document
-      .elementFromPoint(prediction.x, prediction.y)
-      .parentElement.classList.contains("hover") ||
-    document
-      .elementFromPoint(prediction.x, prediction.y)
-      .classList.contains("hover")
-  ) {
-    validator++;
+var validator = 0
+setInterval(function(){
+    prediction = webgazer.getCurrentPrediction()
+    console.log(document.elementFromPoint(prediction.x, prediction.y))
+    if(document.elementFromPoint(prediction.x, prediction.y).parentElement.classList.contains("hover") || document.elementFromPoint(prediction.x, prediction.y).classList.contains("hover")){
+        
+        validator++    
 
-    if (validator == 3) {
-      document.elementFromPoint(prediction.x, prediction.y).click();
-      validator = 0;
-    }
-  } else validator = 0;
-}, 1000);
+        if(validator == 3){
+            document.elementFromPoint(prediction.x, prediction.y).click()
+            validator = 0
+        }
+    }    
+    else
+        validator = 0
+    
+}, 1000)
 
-function painting() {
-  emptyList();
-  addElement("top-page");
-  addToMap("top-page", function () {
-    gotoPage("/settings.html?=drama", "top");
-  });
 
-  addElement("bottom-page");
-  addToMap("bottom-drama", function () {
-    gotoPage("/apps.html", "bottom");
-  });
 
-  //addElement("right-drama");
-  addToMap("right-drama", function () {});
 
-  //addElement("left-drama");
-  addToMap("left-drama", function () {});
+function painting()
+{
+    emptyList();
+    addElement("top-page");
+    addToMap("top-page",function(){
+        gotoPage('/settings.html?=drama','top'); 
+    });
 
-  addElement("gioconda");
-  addToMap("gioconda", function () {
-    openImage("/visualizzazione.html?=painting", "gioconda");
-  });
+    addElement("bottom-page");
+    addToMap("bottom-drama",function(){
+        gotoPage('/apps.html','bottom');
+    });
 
-  addElement("urlo");
-  addToMap("urlo", function () {
-    openImage("/visualizzazione.html?=painting", "urlo");
-  });
+    //addElement("right-drama");
+    addToMap("right-drama",function(){
+    });
 
-  addElement("vangogh");
-  addToMap("vangogh", function () {
-    openImage("/visualizzazione.html?=painting", "vangogh");
-  });
+    //addElement("left-drama");
+    addToMap("left-drama",function(){
+    });
 
-  addElement("nafea");
-  addToMap("nafea", function () {
-    openImage("/visualizzazione.html?=painting", "nafea");
-  });
+    addElement("gioconda");
+    addToMap("gioconda",function(){
+        openImage('/visualizzazione.html?=painting','gioconda');
+    });
 
-  addElement("turbante");
-  addToMap("turbante", function () {
-    openImage("/visualizzazione.html?=painting", "turbante");
-  });
+    addElement("urlo");
+    addToMap("urlo",function(){
+        openImage('/visualizzazione.html?=painting','urlo');
+    });
 
-  addElement("dama");
-  addToMap("dama", function () {
-    openImage("/visualizzazione.html?=painting", "dama");
-  });
+    addElement("vangogh");
+    addToMap("vangogh",function(){
+        openImage('/visualizzazione.html?=painting','vangogh');
+    });
+
+    addElement("nafea");
+    addToMap("nafea",function(){
+        openImage('/visualizzazione.html?=painting','nafea');
+    });
+
+    addElement("turbante");
+    addToMap("turbante",function(){
+        openImage('/visualizzazione.html?=painting','turbante');
+    });
+
+    addElement("dama")
+    addToMap("dama",function(){
+        openImage('/visualizzazione.html?=painting','dama');
+    });
 }
 
-function settingsload() {
-  emptyList();
-  addElement("bottom-settings");
-  addToMap("bottom-settings", function () {
-    if (window.location.href.indexOf("?=") < 0) {
-      gotoPage("/painting.html", "bottom");
-    } else {
-      gotoPage(
-        "/" +
-          window.location.href.substring(
-            window.location.href.indexOf("?=") + 2
-          ) +
-          ".html",
-        "bottom"
-      );
-    }
-  });
+function settingsload()
+{
+    emptyList();
+    addElement("bottom-settings");
+    addToMap("bottom-settings", function(){
+        if(window.location.href.indexOf('?=') < 0)
+        {
+            gotoPage('/painting.html','bottom');
+        }
+        else
+        {
+            gotoPage('/' + window.location.href.substring(window.location.href.indexOf('?=') + 2) + '.html','bottom');
+        }
+    });
 
-  addElement("down-bright");
-  addToMap("down-bright", function () {
-    decreaseBrightness();
-  });
+    addElement("down-bright");
+    addToMap("down-bright", function(){
+        decreaseBrightness();
+    });
 
-  addElement("up-bright");
-  addToMap("up-bright", function () {
-    increaseBrightness();
-  });
+    addElement("up-bright");
+    addToMap("up-bright", function(){
+        increaseBrightness();
+    });
 }
 
-function visual() {
-  emptyList();
-  addElement("top-image");
-  addToMap("top-image", function () {
-    gotoPage("/settings.html?=image", "top");
-  });
+function visual()
+{
+    emptyList();
+    addElement("top-image");
+    addToMap("top-image",function(){
+        gotoPage('/settings.html?=image','top');
+    });
 
-  addElement("bottom-image");
-  addToMap("bottom-image", function () {
-    gotoPage("/painting.html", "bottom");
-  });
+    addElement("bottom-image");
+    addToMap("bottom-image",function(){
+            gotoPage('/painting.html','bottom');
+    });
 
-  addElement("right-image");
-  addToMap("right-video", function () {
-    quadroAvanti();
-  });
+    addElement("right-image");
+    addToMap("right-video",function(){
+        quadroAvanti();
+    });
 
-  addElement("left-image");
-  addToMap("left-video", function () {
-    quadroIndietro();
-  });
-
-  addElement("pallino");
-  addElement("pallino2");
+    addElement("left-image");
+    addToMap("left-video",function(){
+        quadroIndietro();
+    });
+    
+    addElement("pallino");
+    addElement("pallino2");
 }
 
 function apps() {
-  emptyList();
-  addElement("painting");
-  addToMap("painting", function () {
-    gotoPage("/painting.html", "top");
-  });
+    emptyList();
+    addElement("painting");
+    addToMap("painting",function(){
+        gotoPage('/painting.html','top');
+    });
 
-  addElement("apps-top");
-  addToMap("apps-top", function () {
-    gotoPage("/settings.html?=apps", "top");
-  });
+    addElement("apps-top");
+    addToMap("apps-top",function(){
+        gotoPage('/settings.html?=apps','top')
+    })
+
+
 }
 
-document.onkeypress = function (e) {
-  if (e.key == " ") {
-    let hovered = document.getElementsByClassName("hover")[0];
-    func = myMap.get(hovered.id.toString());
-    func();
-  }
-};
-
-var csvFileData = new Array();
-
-setInterval(function () {
-  prediction = webgazer.getCurrentPrediction();
-  var value = [prediction.x, prediction.y];
-  csvFileData.push(value);
-  console.log(csvFileData);
-}, 100);
-
-function download_csv_file() {
-  //define the heading for each row of the data
-  var csv = "X position, Y position\n";
-  //merge the data with CSV
-  csvFileData.forEach(function (row) {
-    csv += row.join(",");
-    csv += "\n";
-  });
-
-  //display the created CSV data on the web browser
-  document.write(csv);
-
-  var hiddenElement = document.createElement("a");
-  hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURI(csv);
-  hiddenElement.target = "_blank";
-
-  //provide the name for the CSV file to be downloaded
-  hiddenElement.download = "Gaze positions.csv";
-  hiddenElement.click();
+document.onkeypress = function(e) {
+    if(e.key == ' ')
+    {
+        let hovered = document.getElementsByClassName("hover")[0];
+        func = myMap.get(hovered.id.toString());
+        func();        
+    }
 }
+
+var csvFileData = new Array
+
+
+setInterval(function(){
+    prediction = webgazer.getCurrentPrediction()
+    var value = [prediction.x, prediction.y]
+    csvFileData.push(value)
+    console.log(csvFileData)
+}, 100)
+
+ function download_csv_file() {  
+    //define the heading for each row of the data  
+    var csv = 'X position, Y position\n';  
+    //merge the data with CSV  
+    csvFileData.forEach(function(row) {  
+            csv += row.join(',');  
+            csv += "\n";  
+    });  
+   
+    //display the created CSV data on the web browser   
+    document.write(csv);  
+    
+    var hiddenElement = document.createElement('a');  
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);  
+    hiddenElement.target = '_blank';  
+      
+    //provide the name for the CSV file to be downloaded  
+    hiddenElement.download = 'Gaze positions.csv';  
+    hiddenElement.click();  
+
+}  
+
